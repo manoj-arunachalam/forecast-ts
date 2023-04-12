142d9466ae534927e22c8a738b511b2d46245b7d
Manoj A <magmanoj7@gmail.com>
2023-04-12 20:10:49 +0530

    some changes

--- app.py
+++ app.py
@@ -31,13 +31,27 @@ def predict():
 	# Create the future dataframe for prediction
 	future_df = model.make_future_dataframe(periods=periods)
 
+	#Global definitions
+	global start_time
+	global end_time
+	global execution_time
+	execution_time = 0
+
 	# Make predictions
 	start_time = time.time()
 	forecast = model.predict(future_df)
 	end_time = time.time()
 
+	execution_time = end_time - start_time
+
 	# Return the forecast as a JSON response
 	return jsonify((forecast[['ds', 'yhat']].tail(periods)).to_dict(orient='records'))
+
+# Define a new function for a new page
+@app.route('/execute')
+def execute():
+	about_text = "Time taken to complete "+ str(execution_time)
+	return render_template('execute.html', about_text=about_text)
 
 # Run the app
 if __name__ == '__main__':
